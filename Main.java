b635869781992a2dbfbd4c1bf8e74bbf9225bdf7
ThomasKenyeres <kenyerest97@gmail.com>
2017-01-21 16:25:48 +0100

    Main changed

--- Main.java
+++ Main.java
@@ -2,11 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Garbage g = new Garbage("kakiee");
-        System.out.println("HEY");
-        System.out.println(g.getName());
-
-        PaperGarbage pg = new PaperGarbage("szlotty", true);
-        System.out.println(pg.getName());
+        PaperGarbage pgarb = new PaperGarbage("szemet", false);
+        System.out.println(pgarb.getName());
     }
 }
